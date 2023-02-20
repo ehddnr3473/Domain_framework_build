36fb96216ae7811464b79b1b176d1d799a79a6cc
Donguk Kim <ehddnr73@naver.com>
2023-02-21 08:30:07 +0900

    리팩터링 한 코드 archving & create xcframework

---
+++ Domain/build/iphoneos/objroot/Domain.build/Release-iphoneos/Domain.build/Objects-normal/arm64/Domain_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/kimdonguk/Desktop/Domain_framework_build/Domain/build/iphoneos/objroot/Domain.build/Release-iphoneos/Domain.build/DerivedSources/Domain_vers.c
---
+++ Domain/build/iphonesimulator/objroot/Domain.build/Release-iphonesimulator/Domain.build/Objects-normal/x86_64/Domain_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/kimdonguk/Desktop/Domain_framework_build/Domain/build/iphonesimulator/objroot/Domain.build/Release-iphonesimulator/Domain.build/DerivedSources/Domain_vers.c
