a531ff237fd1ec3744cc230de35c95f171b7bc5d
Donguk Kim <ehddnr73@naver.com>
2023-02-25 14:36:58 +0900

    Archiving & make xcframework

--- Domain/build/iphoneos/objroot/Domain.build/Release-iphoneos/Domain.build/Objects-normal/arm64/Domain_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kimdonguk/Desktop/Domain_framework_build/Domain/build/iphoneos/objroot/Domain.build/Release-iphoneos/Domain.build/DerivedSources/Domain_vers.c
--- Domain/build/iphonesimulator/objroot/Domain.build/Release-iphonesimulator/Domain.build/Objects-normal/x86_64/Domain_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kimdonguk/Desktop/Domain_framework_build/Domain/build/iphonesimulator/objroot/Domain.build/Release-iphonesimulator/Domain.build/DerivedSources/Domain_vers.c
