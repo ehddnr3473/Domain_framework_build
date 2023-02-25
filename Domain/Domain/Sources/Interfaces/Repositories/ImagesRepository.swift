62bfc6cec0689c777563e45f7596b9277c60973d
Donguk Kim <ehddnr73@naver.com>
2023-02-25 14:21:53 +0900

    인터페이스 이름 변경

--- Domain/Domain/Sources/Interfaces/Repositories/ImagesRepository.swift
+++ Domain/Domain/Sources/Interfaces/Repositories/ImagesRepository.swift
@@ -1,5 +1,5 @@
 //
-//  AbstractImageRepository.swift
+//  ImagesRepository.swift
 //  Domain
 //
 //  Created by 김동욱 on 2023/02/18.
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-public protocol AbstractImageRepository: AnyObject {
+public protocol ImagesRepository: AnyObject {
     func upload(at index: Int, _ image: UIImage) async throws
     func read(at index: Int, _ completion: @escaping ((Result<UIImage, Error>) -> Void))
     func delete(at index: Int) async throws
