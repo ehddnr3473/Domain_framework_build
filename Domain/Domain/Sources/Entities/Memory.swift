d9d60e8ed2d9207d35c63b3e7d2986d8eb22f16b
Donguk Kim <ehddnr73@naver.com>
2023-02-27 20:13:16 +0900

    Hashable 프로토콜 적합성 추가

--- Domain/Domain/Sources/Entities/Memory.swift
+++ Domain/Domain/Sources/Entities/Memory.swift
@@ -7,14 +7,15 @@
 
 import Foundation
 
-public struct Memory {
+public struct Memory: Hashable {
+    public typealias Identifier = Int
+    public let id: Identifier
     public let title: String
-    public let index: Int
     public let uploadDate: Date
     
-    public init(title: String, index: Int, uploadDate: Date) {
+    public init(id: Int, title: String, uploadDate: Date) {
+        self.id = id
         self.title = title
-        self.index = index
         self.uploadDate = uploadDate
     }
 }
