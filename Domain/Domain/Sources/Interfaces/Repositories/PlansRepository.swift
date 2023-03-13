0510031f4c765dc05e31e7658a2765f6ead2a5ff
Donguk Kim <ehddnr73@naver.com>
2023-03-13 19:18:48 +0900

    PlansRepository interface 메서드 시그니처 수정

--- Domain/Domain/Sources/Interfaces/Repositories/PlansRepository.swift
+++ Domain/Domain/Sources/Interfaces/Repositories/PlansRepository.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public protocol PlansRepository {
-    func upload(key: String, plan: Plan) async throws
+    func upload(plan: Plan) async throws
     func read() async throws -> [Plan]
-    func delete(key: String, plans: [Plan]) async throws
+    func delete(key: String) async throws
     func swap(_ swapPlansBox: SwapPlansBox) async throws
 }
