e1b56ef5484e7c16559001b645277e72cac9720b
Donguk Kim <ehddnr73@naver.com>
2023-03-15 02:58:20 +0900

    Coordinate struct 정의

---
+++ Domain/Domain/Sources/Entities/Coordinate.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinate.swift
+//  Domain
+//
+//  Created by 김동욱 on 2023/03/15.
+//
+
+import Foundation
+
+public struct Coordinate {
+    let latitude: Double
+    let longitude: Double
+}
